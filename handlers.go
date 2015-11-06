27dbad0048e7dd16696cf4bdfce0e9719b47eea3
Remo Samuel Paul <remosam42@gmail.com>
2015-11-06 10:00:28 +0300

    Initial commit testing go

---
+++ handlers.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "encoding/json"
+    "time"
+    "github.com/gorilla/mux"
+)
+
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "encoding/json"
+    "time"
+    "github.com/gorilla/mux"
+)
+
+
+func Index(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w, "Welcome !!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request)  {
+  todos := Todos{
+        Todo{Name: "Write presentation"},
+        Todo{Name: "Host meetup"},
+    }
+
+    json.NewEncoder(w).Encode(todos)
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    todoId := vars["todoId"]
+    fmt.Fprintln(w, "Todo show:", todoId)
+}
+
+
+func main() {
+
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", Index)
+    router.HandleFunc("/todos", TodoIndex)
+    router.HandleFunc("/todos/{todoId}", TodoShow)
+
+
+    log.Fatal(http.ListenAndServe(":8080", router))
+
+}
+
+
+
+type Todo struct{
+  Name      string `json:"name"`
+  Completed bool    `json:"completed"`
+  Due       time.Time `json:"due"`
+}
+
+type Todos []Todo
